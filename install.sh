2a55af30368dbd14c1bf10f2dcc473b8a1f443f0
Thomas Holst <2027794+tholst@users.noreply.github.com>
2019-08-31 00:18:04 +0200

    some adaptations; mostly git aliases

--- install.sh
+++ install.sh
@@ -15,7 +15,7 @@ homedir=$1
 dotfiledir=${homedir}/dotfiles
 
 # list of files/folders to symlink in ${homedir}
-files="bash_profile bashrc bash_prompt aliases private"
+files="bash_profile bashrc bash_prompt aliases gitconfig"
 
 # change to the dotfiles directory
 echo "Changing to the ${dotfiledir} directory"
